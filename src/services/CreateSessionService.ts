27c2f0e49e5a083291321713d13db497bc78d213
Mayko Kioschi <mkioschi@gmail.com>
2020-11-17 23:01:56 -0300

    Create authenticate route

---
+++ src/services/CreateSessionService.ts
@@ -0,0 +1,46 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import User from '../models/User';
+
+interface CreateSessionDTO {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class CreateSessionService {
+  public async execute({
+    email,
+    password,
+  }: CreateSessionDTO): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new Error('Email or password does not match.');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Email or password does not match.');
+    }
+
+    const token = sign({}, '40fdc6104a9803bd065714f42aefd9e5', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
+  }
+}
+
+export default CreateSessionService;
