06aa90ea778b84defc508fe70fb4bd8507c4320e
Mayko Kioschi <mkioschi@gmail.com>
2020-11-19 00:33:12 -0300

    Implements upload avatar of users

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 import './database';
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use('/files', express.static(uploadConfig.directory));
 
 app.listen(3333, () => {
   console.log('🍺 Server started on port 3333!');
